b2a8e6093343e97a0a1b070ce250b304aaf834f6
Jahangir Ashraf Nasirwala <acerdesktop53@gmail.com>
2023-02-22 00:52:32 +0530

    Implementing UseState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM  from "react";
 import "./App.css";
 
 function App(){
+  const [count, setCount] = useState(0)
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="App">
+            <header>
+                <h1>Counter App using State</h1>
+            </header>
+            <h2>Current Value of count is {count}</h2>
+            <button onClick={ () => setCount(0)}>Reset Counter</button>
+            <button onClick={ () => setCount(count+1)}>Increase Counter</button>
+            <button onClick={ () => count<=0 ? "": setCount(count-1)}>Dicrease Counter</button>
         </div>
     );
 };
